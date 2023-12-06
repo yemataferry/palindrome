e22cc122da0c5fc6e21d0eb1e3f54b4e49fc5f43
Yemata Ferry <yemataferry@gmail.com>
2023-12-06 21:29:32 +0100

    style and js done

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,22 +1,22 @@
-function palindrome(str) {
-  let j = str.length - 1;
-  for (let i = 0; i < str.length / 2; i++) {
+function palindrome (str) {
+  let j = str.length - 1
+  for (let i = 0; i < j / 2; i++) {
     if (str[i] === str[j]) {
-      return true;
+      return true
     }
-    return false;
+    return false
   }
 }
 // console.log(palindrome("baab"));
 
-function checkPalindrome() {
-  const word = document.getElementById("question").value;
+function checkPalindrome () {
+  const word = document.getElementById('question').value;
   const palindrom = palindrome(word);
-  const answer = document.getElementById("result");
+  const answer = document.getElementById('result')
 
   if (palindrom) {
-    answer.innerHTML = "The word entered is a Palindrome";
+    answer.innerHTML = 'The word entered is a Palindrome'
   } else {
-    answer.innerHTML = "The word entered is not a Palindrome";
+    answer.innerHTML = 'The word entered is not a Palindrome'
   }
 }
