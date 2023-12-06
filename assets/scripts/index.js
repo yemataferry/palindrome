3867a46bf965de02e2e29e766f5ae9859267ff8e
Yemata Ferry <yemataferry@gmail.com>
2023-12-06 21:20:04 +0100

    style and js done

---
+++ assets/scripts/index.js
@@ -0,0 +1,22 @@
+function palindrome(str) {
+  let j = str.length - 1;
+  for (let i = 0; i < str.length / 2; i++) {
+    if (str[i] === str[j]) {
+      return true;
+    }
+    return false;
+  }
+}
+// console.log(palindrome("baab"));
+
+function checkPalindrome() {
+  const word = document.getElementById("question").value;
+  const palindrom = palindrome(word);
+  const answer = document.getElementById("result");
+
+  if (palindrom) {
+    answer.innerHTML = "The word entered is a Palindrome";
+  } else {
+    answer.innerHTML = "The word entered is not a Palindrome";
+  }
+}
