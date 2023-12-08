bfbd8987c77ee91774d4472d62d740988f9e11d4
Yemata Ferry <yemataferry@gmail.com>
2023-12-08 14:43:11 +0100

    palindrome set and done

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,14 +1,14 @@
 function palindrome (str) {
-  let array = str.split(' ').join('')
-   let j = array.length - 1
-    for (let i = 0; i <= j / 2; i++) {
-      if (array[i] !== array[j]) {
-        return false
-      }
-      j--
+  const array = str.split(' ').join('')
+  let j = array.length - 1
+  for (let i = 0; i <= j / 2; i++) {
+    if (array[i] !== array[j]) {
+      return false
     }
-    return true
+    j--
   }
+  return true
+}
 
 console.log(palindrome('do gees see god'))
 
